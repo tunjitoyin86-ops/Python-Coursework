58e3bbcbc7049783c9f6c041d667790973bf6a2e
tunjitoyin86-ops <tunjitoyin86@gmail.com>
2026-04-13 12:29:04 -0700

    Add files via upload

---
+++ week_1.py
@@ -0,0 +1,13 @@
+#Create a variable 'x' and assign it the value 10
+x = 10
+
+#Create a variable 'name' and assign it a string
+name = "Alice"
+
+#create a variable 'pi' and assign it a float value
+pi = 3.14159
+
+
+
+
+
